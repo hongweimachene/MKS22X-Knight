603a7aaa9d68a5de3b959a77877e12dddfcf5ae5
hongweimachene <hchen00@stuy.edu>
2019-02-13 11:44:16 -0500

    started helper addKnight

--- KnightBoard.java
+++ KnightBoard.java
@@ -10,8 +10,13 @@ public class KnightBoard{
     String s = "";
     for (int i = 0; i < board.length; i++) {
       for (int j = 0; j < board[i].length; j++) {
-        s+=board[i][j];
+        if (board[i][j] > 9) {
+          s+=board[i][j] + " ";
+        } else {
+          s+= " " + board[i][j] + " ";
+        }
       }
+      s+="\n";
     }
     return s;
   }
@@ -25,13 +30,30 @@ public class KnightBoard{
 
   }
 
+  private boolean solveH(int startingRow, int startingCol, int level) {
+
+  }
+
+  private boolean addKnight(int row, int col) {
+    int[][] moves = new int[][] {
+      {row+2, col-1},
+      {row+2, col+1},
+      {row-2, col-1},
+      {row-2, col+1},
+      {row-1, col+2},
+      {row+1, col+2},
+      {row-1, col-2},
+      {row+1, col-2}
+    };
+    
+  }
   /**
   *@throws IllegalStateException when the board contains non-zero values.
   *@throws IllegalArgumentException when either parameter is negative or out of bounds.
   *@return the number of solutions from the starting position specified
   */
   public int countSolutions(int startingRow, int startingCol) {
-    
+
   }
 
 

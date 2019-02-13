e9ab54479893af7783e03639e199e9463599d594
hongweimachene <hchen00@stuy.edu>
2019-02-13 11:26:35 -0500

    added empty solve functions

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,9 +1,11 @@
 public class KnightBoard{
-  private int[] board;
-  /** @throws IllegalArgumentException when either parameter is <= 0.**/
+  private int[][] board;
+
+  /** @throws IllegalArgumentException when either parameter is <= 0.*/
   public KnightBoard(int startingRows,int startingCols) {
-    board = new[startingRows][startingCols];
+    board = new int[startingRows][startingCols];
   }
+
   public String toString() {
     String s = "";
     for (int i = 0; i < board.length; i++) {
@@ -13,11 +15,24 @@ public class KnightBoard{
     }
     return s;
   }
+
   /**
-    *@throws IllegalStateException when the board contains non-zero values.
-    *@throws IllegalArgumentException when either parameter is negative or out of bounds.
-    *@return true when the board is solvable from the specified starting position
+  *@throws IllegalStateException when the board contains non-zero values.
+  *@throws IllegalArgumentException when either parameter is negative or out of bounds.
+  *@return true when the board is solvable from the specified starting position
   */
-  
+  public boolean solve(int startingRow, int startingCol) {
+
+  }
+
+  /**
+  *@throws IllegalStateException when the board contains non-zero values.
+  *@throws IllegalArgumentException when either parameter is negative or out of bounds.
+  *@return the number of solutions from the starting position specified
+  */
+  public int countSolutions(int startingRow, int startingCol) {
+    
+  }
+
 
 }

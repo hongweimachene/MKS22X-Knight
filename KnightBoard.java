7fd9150d1eba05e628f80e9cd6ffce02ea916f26
hongweimachene <hchen00@stuy.edu>
2019-02-25 11:42:40 -0500

    worked on solve helper

--- KnightBoard.java
+++ KnightBoard.java
@@ -10,7 +10,7 @@ public class KnightBoard{
     String s = "";
     for (int i = 0; i < board.length; i++) {
       for (int j = 0; j < board[i].length; j++) {
-        if (board[i][j] > 9) {
+        if (board[i][j] > 0) {
           s+=board[i][j] + " ";
         } else {
           s+= " " + board[i][j] + " ";
@@ -27,11 +27,11 @@ public class KnightBoard{
   *@return true when the board is solvable from the specified starting position
   */
   public boolean solve(int startingRow, int startingCol) {
-
+    return solveH(startingRow, startingCol, 1);
   }
 
   private boolean solveH(int startingRow, int startingCol, int level) {
-    if (level == startingRow * startingCol) {
+    if (level == board.length * board[0].length) {
       return true;
     }
     int[][] moves = new int[][] {
@@ -45,21 +45,23 @@ public class KnightBoard{
       {startingRow+1, startingCol-2}
     };
     for (int i = 0; i < moves.length; i++) {
-      if (moves[i][0] < board.length && moves[i][0] >= 0 && moves[i][1] < board[0].length && moves[i][1] >= 0) {
-        
+      if (addKnight(level, moves, i)) {
+        return solveH(moves[i][0], moves[i][1], level+1);
       }
     }
+    return false;
   }
 
-  private boolean addKnight(int row, int col, int level, int[][] moves) {
-    if (moves[i][0] < board.length && moves[i][0] >= 0 && moves[i][1] < board[0].length && moves[i][1] >= 0) {
-      if (board[moves[i][0]][moves[i][1]] == 0) {
-        board[moves[i][0]][moves[i][1]] = level;
+  private boolean addKnight(int level, int[][] moves, int move) {
+    if (moves[move][0] < board.length && moves[move][0] >= 0 && moves[move][1] < board[0].length && moves[move][1] >= 0) {
+      if (board[moves[move][0]][moves[move][1]] == 0) {
+        board[moves[move][0]][moves[move][1]] = level;
         return true;
       }
     }
     return false;
   }
+
   /**
   *@throws IllegalStateException when the board contains non-zero values.
   *@throws IllegalArgumentException when either parameter is negative or out of bounds.

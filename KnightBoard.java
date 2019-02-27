10eb302e2d2eb981027e3416dffe57dce6f4b146
hongweimachene <hchen00@stuy.edu>
2019-02-27 00:51:34 -0500

    fixing solve

--- KnightBoard.java
+++ KnightBoard.java
@@ -10,7 +10,7 @@ public class KnightBoard{
     String s = "";
     for (int i = 0; i < board.length; i++) {
       for (int j = 0; j < board[i].length; j++) {
-        if (board[i][j] > 0) {
+        if (board[i][j] > 9) {
           s+=board[i][j] + " ";
         } else {
           s+= " " + board[i][j] + " ";
@@ -44,32 +44,42 @@ public class KnightBoard{
       {startingRow-1, startingCol-2},
       {startingRow+1, startingCol-2}
     };
-    for (int i = 0; i < moves.length; i++) {
-      if (addKnight(level, moves, i)) {
-        return solveH(moves[i][0], moves[i][1], level+1);
+    if (addKnight(startingRow, startingCol, level)) {
+      for (int i = 0; i < moves.length; i++) {
+        if (solveH(moves[i][0], moves[i][1], level+1)) return true;
       }
+      removeKnight(startingRow, startingCol);
     }
     return false;
   }
 
-  private boolean addKnight(int level, int[][] moves, int move) {
-    if (moves[move][0] < board.length && moves[move][0] >= 0 && moves[move][1] < board[0].length && moves[move][1] >= 0) {
-      if (board[moves[move][0]][moves[move][1]] == 0) {
-        board[moves[move][0]][moves[move][1]] = level;
+  private boolean addKnight(int row, int col, int level) {
+    if (row < board.length && row >= 0 && col < board[0].length && col >= 0) {
+      if (board[row][col] == 0) {
+        board[row][col] = level;
         return true;
       }
     }
     return false;
   }
 
-  /**
-  *@throws IllegalStateException when the board contains non-zero values.
-  *@throws IllegalArgumentException when either parameter is negative or out of bounds.
-  *@return the number of solutions from the starting position specified
-  */
-  public int countSolutions(int startingRow, int startingCol) {
-
+  private boolean removeKnight(int row, int col) {
+    board[row][col] = 0;
+    return true;
   }
 
+  // /**
+  // *@throws IllegalStateException when the board contains non-zero values.
+  // *@throws IllegalArgumentException when either parameter is negative or out of bounds.
+  // *@return the number of solutions from the starting position specified
+  // */
+  // public int countSolutions(int startingRow, int startingCol) {
+  //
+  // }
+  public static void main(String[] args) {
+    KnightBoard a = new KnightBoard(5,5);
+    System.out.println(a.solve(3,4));
+    System.out.println(a);
+  }
 
 }

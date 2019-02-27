5c0f1cef3847f7042635b172763f6665999980a3
hongweimachene <hchen00@stuy.edu>
2019-02-27 01:54:13 -0500

    created class file for data structure

---
+++ Tile.java
@@ -0,0 +1,18 @@
+public class Tile implements Comparable<Tile> {
+  private int row, col, moves;
+  public Tile(int rows, int cols, int edges) {
+    row = rows;
+    col = cols;
+    moves = edges;
+  }
+  public int getRow() {
+    return row;
+  }
+  public int getCol() {
+    return col;
+  }
+  public int getMoves() {
+    return moves;
+  }
+  
+}

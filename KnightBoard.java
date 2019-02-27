f757a947dd570888515b0881fcbcc401b060f41b
hongweimachene <hchen00@stuy.edu>
2019-02-27 01:26:01 -0500

    added throws to solve and countSolutions

--- KnightBoard.java
+++ KnightBoard.java
@@ -3,6 +3,7 @@ public class KnightBoard{
 
   /** @throws IllegalArgumentException when either parameter is <= 0.*/
   public KnightBoard(int startingRows,int startingCols) {
+    if (startingRows < 0 || startingCols < 0) throw new IllegalArgumentException();
     board = new int[startingRows][startingCols];
   }
 
@@ -21,12 +22,31 @@ public class KnightBoard{
     return s;
   }
 
+  private void clearBoard(){
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[i].length; j++) {
+        board[i][j] = 0;
+      }
+    }
+  }
+  private boolean isEmpty(){
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[i].length; j++) {
+        if (board[i][j] != 0) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
   /**
   *@throws IllegalStateException when the board contains non-zero values.
   *@throws IllegalArgumentException when either parameter is negative or out of bounds.
   *@return true when the board is solvable from the specified starting position
   */
   public boolean solve(int startingRow, int startingCol) {
+    if (!isEmpty()) throw new IllegalStateException();
+    if (startingRow < 0 || startingCol < 0 || startingRow > board.length || startingCol > board[0].length) throw new IllegalArgumentException();
     return solveH(startingRow, startingCol, 1);
   }
 
@@ -74,6 +94,8 @@ public class KnightBoard{
   *@return the number of solutions from the starting position specified
   */
   public int countSolutions(int startingRow, int startingCol) {
+    if (!isEmpty()) throw new IllegalStateException();
+    if (startingRow < 0 || startingCol < 0 || startingRow > board.length || startingCol > board[0].length) throw new IllegalArgumentException();
     return countSolutionsH(startingRow, startingCol, 1);
   }
 
@@ -101,6 +123,8 @@ public class KnightBoard{
     }
     return count;
   }
+
+
 
   public static void main(String[] args) {
     KnightBoard a = new KnightBoard(5,5);
